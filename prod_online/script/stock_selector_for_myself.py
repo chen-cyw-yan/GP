e92b6905564c72012d6b36ff78c7e0ce6f282767
chen-cyw-yan <68014835+chen-cyw-yan@users.noreply.github.com>
2026-04-01 11:02:52 +0800

    跟新

--- prod_online/script/stock_selector_for_myself.py
+++ prod_online/script/stock_selector_for_myself.py
@@ -199,7 +199,8 @@ def main():
         """
     filter_stock.toSql(sql=sql, rows=rows_data)
 
-    last_day=df_tmp.loc[df_tmp['trade_date']].max()
+    last_day=max(df_tmp['trade_date'].to_list())
+    
     today_df=df_tmp.loc[df_tmp['trade_date'].astype(str)==last_day]
     today_df=today_df[['stock_code','stock_name','trigger_count','is_abnormal_type','warning_info','region_block','industry_block','concept_block']]
     today_rows_data = today_df.values.tolist()
